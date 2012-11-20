0a77198cafc92278d6d7184d74527aa710efeb97
Michael Berg <michael.berg@web.de>
2012-11-20 13:17:21 +0100

    test

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]]
+													 
             :main noir-test.server)
 
